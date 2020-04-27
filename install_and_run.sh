454dc89b94ab02204e21dbbe56d5d2842efdb26a
Wojciech Korzybski <46850894+Grzybu1@users.noreply.github.com>
2020-04-27 19:32:21 +0200

    Update install_and_run.sh

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,2 +1,2 @@
 sudo docker build --tag test .
-sudo docker run -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged test
+sudo docker run --rm -it --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged test
